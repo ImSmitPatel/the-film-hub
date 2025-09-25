2026f0179557ca0cba96d864e77d4fc8ee23df0e
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-09-25 20:10:46 +0530

    feat: initial project setup with React and Vite configuration

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Movies from './components/Movies'
+import TVShows from './components/TVShows'
+import WatchList from './components/WatchList'
+
+function App() {
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <div className='sticky top-0 z-50'>
+            <NavBar />
+          </div>
+          <Routes>
+            <Route path='/' element={<Movies />} />
+            <Route path='/tv' element={<TVShows />} />
+            <Route path='/watchlist' element={<WatchList />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
