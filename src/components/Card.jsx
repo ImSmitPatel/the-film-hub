bcacae5c35c1c0449a366c9902065f819959b85e
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-09-27 18:38:23 +0530

    chore: rename a variable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function Card({ movieTitle, movieImage }) {
-  const baseImageUrl = "https://media.themoviedb.org/t/p/w220_and_h330_face";
+  const BASE_IMAGE_URL = "https://media.themoviedb.org/t/p/w220_and_h330_face";
 
   return (
     <div className='flex flex-col items-center'>
       <div
         className='w-[220px] h-[330px] rounded-xl bg-coverbg-center flex items-center justify-center '
-        style={{ backgroundImage: `url(${baseImageUrl}${movieImage})` }}
+        style={{ backgroundImage: `url(${BASE_IMAGE_URL}${movieImage})` }}
       >
       </div>
       <div className="text-black text-xl w-[220px] text-center">
