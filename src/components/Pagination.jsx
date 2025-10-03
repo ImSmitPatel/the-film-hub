f82b2a0d241ded2b207b93f9aeeb3ad717304201
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-10-03 18:09:02 +0530

    sync-repo

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LucideArrowLeft } from 'lucide-react'
+
+function Pagination() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Pagination
